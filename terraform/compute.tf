1bdee59e27b1c990dae75ff0fc7837fc6a35b905
Aryeh <Aryeh.Kuller@gmail.com>
2024-11-06 11:23:15 -0500

    Update formating

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -50,7 +50,7 @@ resource "aws_ecs_task_definition" "factorio_ecs_task_definition" {
     {
       "image": "${var.factorio_docker_image}:${var.factorio_image_tag}",
       "memory": 1024,
-      "name": "factorio"
+      "name": "factorio",
       "portMappings": [
         {
           "containerPort": 34197,
@@ -62,14 +62,14 @@ resource "aws_ecs_task_definition" "factorio_ecs_task_definition" {
           "hostPort": 27015,
           "protocol": "tcp"
         }
-      ]
+      ],
       "mount_points": [
         {
           "ContainerPath": "/factorio",
           "ReadOnly": false,
           "SourceVolume": "factorio"
         }
-      ]
+      ],
       "envrionment" = [
         {"name": "Update MODS on Start", "value": "${var.update_mods_on_start}" },
         {"name": "DLC Space Age", "value": "${var.dlc_space_age}" }
