66fe941299107f69dce8ff2a1a5cef293577da01
Aryeh <Aryeh.Kuller@gmail.com>
2024-11-05 20:29:47 -0500

    Update iam

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -7,6 +7,11 @@ data "aws_iam_policy_document" "instance_assume_role_policy"{
     }
     actions = ["sts:AssumeRole"]
   }
+  statement {
+    Effect = "Allow"
+    Action = "route53:*"
+    Resource = "*"
+  }
 }
 
 resource "aws_iam_role" "instance_role" {
@@ -15,19 +20,6 @@ resource "aws_iam_role" "instance_role" {
   managed_policy_arns = [
     "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
   ]
-  force_detach_policies = {
-    PolicyName = "root"
-    PolicyDocument = {
-      Version = "2012-10-17"
-      Statement = [
-        {
-          Effect = "Allow"
-          Action = "route53:*"
-          Resource = "*"
-        }
-      ]
-    }
-  }
 }
 
 resource "aws_iam_instance_profile" "instance_profile" {
@@ -73,6 +65,17 @@ data "aws_iam_policy_document" "DNS_lambda_policy" {
     }
     actions = ["sts:AssumeRole"]
   }
+
+  Statement {
+    Effect = "Allow"
+    Action = "route53:*"
+    Resource = "*"
+  }
+  statement {
+    Effect = "Allow"
+    Action = "ec2:DescribeInstance*"
+    Resource = "*"
+  }
 }
 
 resource "aws_iam_role" "DNS_lambda_role" {
@@ -80,24 +83,6 @@ resource "aws_iam_role" "DNS_lambda_role" {
   description = "DNS Lambda Policy"
   assume_role_policy = data.aws_iam_policy_document.DNS_lambda_policy
   managed_policy_arns = ["arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"]
-  force_detach_policies = {
-    PolicyName = "root"
-    PolicyDocument = {
-      Version = "2012-10-17"
-      Statement = [
-        {
-          Effect = "Allow"
-          Action = "route53:*"
-          Resource = "*"
-        },
-        {
-          Effect = "Allow"
-          Action = "ec2:DescribeInstance*"
-          Resource = "*"
-        }
-      ]
-    }
-  }
 }
 
 resource "aws_security_group" "factorio-efs-sg" {
