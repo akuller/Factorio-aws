6575225597255742137e054bac871886091eaa89
Aryeh <Aryeh.Kuller@gmail.com>
2024-11-08 15:52:13 -0500

    fix error

--- terraform/networking.tf
+++ terraform/networking.tf
@@ -88,7 +88,7 @@ resource "aws_security_group" "factorio-efs-sg" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = [aws_vpc.main.cidr_block]
+    cidr_blocks = [aws_vpc.factorio_vpc.cidr_block]
   }
 
   egress {
