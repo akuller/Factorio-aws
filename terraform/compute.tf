35b2114efb1ac6ccd2fe95946bf3e19540ec7d0d
Aryeh <Aryeh.Kuller@gmail.com>
2024-11-08 22:51:58 -0500

    remove execution role

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -131,7 +131,7 @@ resource "aws_ecs_task_definition" "factorio_ecs_task_definition" {
   }
 
   # task_role_arn      = aws_iam_role.ecs_task_role.arn
-  execution_role_arn = aws_iam_role.ecs_exec_role.arn
+  # execution_role_arn = aws_iam_role.ecs_exec_role.arn
 }
 
 resource "aws_ecs_service" "factorio_ecs_service" {
