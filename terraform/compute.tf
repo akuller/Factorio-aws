cc54fc50974158e0a990522ccd817e2673529ea7
Aryeh <Aryeh.Kuller@gmail.com>
2024-11-08 22:22:58 -0500

    update role arn

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -129,7 +129,7 @@ resource "aws_ecs_task_definition" "factorio_ecs_task_definition" {
       transit_encryption = "ENABLED"
     }
   }
-  execution_role_arn = "arn:aws:iam::aws:policy/aws-service-role/AmazonECSServiceRolePolicy"
+  execution_role_arn = "arn:aws:iam::525625957308:role/aws-service-role/ecs.amazonaws.com/AWSServiceRoleForECS"
   # task_role_arn      = aws_iam_role.ecs_task_role.arn
   # execution_role_arn = aws_iam_role.ecs_exec_role.arn
 }
